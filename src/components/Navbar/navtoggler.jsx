b49887700ccf0c5cde59eb47137c72843be4811e
vivekrawat1998 <vr90888561@gmail.com>
2025-02-25 12:58:20 +0530

    fixed

--- src/components/Navbar/navtoggler.jsx
+++ src/components/Navbar/navtoggler.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { useDispatch } from "react-redux";
-import { toggleMenu } from "../../state/menuSlice";
+import { toggleMenu } from "../../state/Menuslice.jsx";
 
 const NavbarToggler = () => {
   const dispatch = useDispatch();
